42fd2d80ad88fe62ca82524312e65e034f4e2936
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2022-01-13 20:15:30 +0900

    初期化時、Dockerコンテナが実行中でなければテーマのインストールコマンドを実行しないように

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 ln -s ../../themes/my_theme data/themes/
-sudo docker-compose exec redmine redmine-install-themes
+
+sudo docker-compose ps | grep Up | grep -q redmine_redmine_1
+if [ $? = 0 ]; then
+    echo sudo docker-compose exec redmine redmine-install-themes
+fi
