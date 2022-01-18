acf3cc46065625cf0af27b7554c5100acffb1c34
HAYASHI Masayuki <masayuki.hayashi.mcc@gmail.com>
2022-01-18 20:17:28 +0900

    テーマのインストールが動いてなかった

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ cp -af themes/my_theme data/themes/
 
 sudo docker-compose ps | grep Up | grep -q redmine_redmine_1
 if [ $? = 0 ]; then
-    echo sudo docker-compose exec redmine redmine-install-themes
+    sudo docker-compose exec redmine redmine-install-themes
 fi
